983cde60b76136b3ccfa8a7b47e647ed4210d312
Leandro <ljborges76@gmail.com>
2023-10-19 14:07:43 -0300

    meu primeiro projeto em java

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual : " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual : " + smartTv.canal);
+
+        System.out.println("TV ligada ? " + smartTv.ligada);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+    }
+}
